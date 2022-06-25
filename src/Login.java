1594d054c70c8d7f4e3d3b79b52191c15543ebdc
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-06-25 14:56:52 +0530

    Neeraj Push

---
+++ src/Login.java
@@ -0,0 +1,14 @@
+ class Login extends Main {
+    public static void login(){
+        String ignore = sc.nextLine();
+        System.out.println("Your Library Username: ");
+        String libraryusername = sc.nextLine();
+        System.out.println("Password: ");
+        String librarypassword = sc.nextLine();
+        //
+        //
+        //
+        //
+        //
+    }
+}
