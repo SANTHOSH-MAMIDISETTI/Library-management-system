d6479d3f8342a95d09c193816e5e0fe094321892
Santhosh Mamidisetti <mamidisettisanthosh2004@gmail.com>
2022-06-27 17:31:39 +0530

    updated date module

--- src/Current_dateTime.java
+++ src/Current_dateTime.java
@@ -6,7 +6,7 @@ public class Current_dateTime extends Main
     public static void current_dateTime()
     {
         LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy            HH:mm:ss");
         String formatDateTime = now.format(formatter);
         System.out.println(formatDateTime);
     }
