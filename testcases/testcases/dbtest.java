4b165e9407cd951630f5efe31f9b5dd845f24191
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-07-06 18:59:06 +0530

    neeraj before 7th

--- testcases/testcases/dbtest.java
+++ testcases/testcases/dbtest.java
@@ -1,27 +1,83 @@
+// package testcases;
+// import java.sql.*;
+// import java.util.HashMap;
+// ;
+// public class dbtest {
+//     public static void main (String[]args) throws Exception {
+//         HashMap hm = new HashMap<>();
+//         String url = "jdbc:mysql://localhost:3306//neerajdb";
+//         String username = "root";
+//         String password = "12345678";
+//         String query = "select * from engineering_books";
+//         Class.forName("com.mysql.jdbc.Driver");
+//         Connection connection = DriverManager.getConnection(url, username, password);
+//         Statement st = connection.createStatement();
+//         ResultSet rs = st.executeQuery(query);
+//         while (rs.next()) {
+//             hm.put(rs.getString("Title"), rs.getString("Author"), rs.getString("Publisher"), rs.getString("Year_"), rs.getString("Languages"), rs.getString("File_"));
+//          }
 
+//         System.out.println(hm);
+//     }
+// }
+// , rs.getInt("Pages")
 package testcases;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql. ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.HashMap;
 
+//import dnl.utils.text.table.TextTable;
+//import net.proteanit.sql.DbUtils;
+import java.util.Formatter;
 public class dbtest {
+    static String s;
+static String s2;
+    public static void main (String[]args) throws Exception {
+        String url = "jdbc:mysql://localhost:3306/neerajdb";
+        String username = "root";
+        String password = "12345678";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(url, username, password);
+        String query = "SELECT * FROM neerajdb.engineering_books";
+        Object query1 = "SELECT * FROM neerajdb.engineering_books";
+        System.out.println("_______________________________________________");
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        rs.next();
+        Formatter fmt = new Formatter();
+        s = String.format("%20s %20s %15s %20s %20s %15s","Name", "Author", "Pages", "Publisher", "Year", "File");
+        System.out.println(s+"\n");
+int i =0;
+        while(i<30){
 
-            public static void main(String[] args) throws Exception {
-                String url = "jdbc:mysql://localhost:3306/neerajdb";
+            String name = rs.getString(1);
+            String Author = rs.getString(2);
+            int Page = rs.getInt(6);
+            String Page1=String.valueOf(Page);
+            String Publisher = rs.getString(7);
+            int year = rs.getInt(8);
+            String year1=String.valueOf(year);
+            String file = rs.getString(10);
+            s2 = String.format("%20s %20s %11s %18s %15s %15s %15s",name, Author, Page1, "    ",Publisher, year1, file);
+            rs.next();
+            i = i+1;
 
-                String uname = "root";
-                String pass = "12345678";
-                Class. for Name("com.mysql.cj.jdbc.Driver");
-                Connection con = DriverManager.getConnection(url, uname, pass);
-                Statement st = con.createStatement();
-                String query = "SELECT * FROM neerajdb.engineering_books";
-                ResultSet rs = st.executeQuery(query);
-                while (rs.next()) {
-                    System.out.println(rs.getInt(columnIndex:1));
-                }
-                st.close();
-                con.close();
-            }
+
+            System.out.println(s2+"\n");
         }
 
+//        while (rs.next()) {
+////            int Year_ = rs.getInt("Year_");
+////            String Title = rs.getString("Title");
+////            String Author = rs.getString("Author");
+////            int pages = rs.getInt("Pages");
+////            String Publisher = rs.getString("Publisher");
+////            String Language = rs.getString("Languages");
+////            rs.next();
+////            System.out.format("%s, %s, %s, %sn", Year_, Title,
+////                    Author, pages);
+//
+//        }
+// + " -> " + rs.getString(5)+
+
+    }
+}
